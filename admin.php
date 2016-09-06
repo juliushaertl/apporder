2fc0b8c0342f1fcc3dca684a4da00074cfb60263
Julius Haertl <jus@bitgrid.net>
2016-09-06 13:02:40 +0200

    Fix blank admin page

--- admin.php
+++ admin.php
@@ -23,8 +23,5 @@
 
 namespace OCA\AppOrder;
 
-use \OCA\AppOrder\AppInfo\Application;
-
-$app = new Application(array());
-$response = $app->getContainer()->query('\OCA\AppOrder\Controller\SettingsController')->adminIndex();
+$response = \OC::$server->query('\OCA\AppOrder\Controller\SettingsController')->adminIndex();
 return $response->render();
